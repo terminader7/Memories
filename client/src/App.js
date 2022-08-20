3ee045e27fe5cba928651d8f44faae32e7593329
Nader Ebrahim <nebrahim2424@gmail.com>
2022-08-19 23:35:32 -0700

    Set up basic file structure for client side of Application. Deleted unnecessary files

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
-import styled from "styled-components";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+
+import { Posts } from "./components/Posts/Posts";
+import { Form } from "./components/Form/Form";
+import memories from "./images/memories.png";
 
 export const App = () => {
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography varianat="h2" aligh="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch">
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Post } from "./Post/Post";
+
+export const Posts = () => {
+  return (
+    <>
+      <h1>POSTS</h1>
+      <Post />
+      <Post />
+    </>
+  );
+};
