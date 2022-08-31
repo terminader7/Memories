46bc5d22eaee11fff0a75debf1e42ccb6b2629f8
Nader Ebrahim <nebrahim2424@gmail.com>
2022-08-31 10:26:59 -0700

    Set up form component structure and styling

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,12 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
+import FileBase from "react-file-base64";
 import useStyles from "./styles";
 
 export const Form = () => {
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
   const classes = useStyles();
 
+  const handleSubmit = () => {};
+
+  const clear = () => {};
+
   return (
-    <>
-      <></>
-    </>
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={`${classes.root} ${classes.form}`}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Creating a Memory</Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            //Line 34 is how you set state using an object. spread operator, then set key to e.target.value
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        ></TextField>
+        <TextField
+          name="title"
+          variant="outlined"
+          label="Title"
+          fullWidth
+          value={postData.title}
+          onChange={(e) =>
+            //Line 34 is how you set state using an object. spread operator, then set key to e.target.value
+            setPostData({ ...postData, title: e.target.value })
+          }
+        ></TextField>
+        <TextField
+          name="message"
+          variant="outlined"
+          label="Message"
+          fullWidth
+          value={postData.message}
+          onChange={(e) =>
+            //Line 34 is how you set state using an object. spread operator, then set key to e.target.value
+            setPostData({ ...postData, message: e.target.value })
+          }
+        ></TextField>
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="Tags"
+          fullWidth
+          value={postData.tags}
+          onChange={(e) =>
+            //Line 34 is how you set state using an object. spread operator, then set key to e.target.value
+            setPostData({ ...postData, tags: e.target.value })
+          }
+        ></TextField>
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({ base64 }) =>
+              setPostData({ ...postData, selectedFile: base64 })
+            }
+          />
+        </div>
+        <Button
+          className={classes.buttonSubmit}
+          vairant="container"
+          color="primary"
+          size="large"
+          type="submit"
+          fullWidth
+        >
+          Submit
+        </Button>
+        <Button
+          vairant="contained"
+          color="secondary"
+          size="small"
+          onClick={clear}
+          fullWidth
+        >
+          Clear
+        </Button>
+      </form>
+    </Paper>
   );
 };
