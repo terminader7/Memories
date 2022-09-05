99ee2ce67c3383aeab5da0fd8fa205cad1439bde
Nader Ebrahim <nebrahim2424@gmail.com>
2022-09-05 10:13:00 -0700

    changed reducer values from strings to actionType variables, stored and hid connection url

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import cors from "cors";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,8 +15,8 @@ app.use(cors()); //app.use cors needs to be above our routes
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://Nader:nm3AfMQsHsSeRkGs@cluster.qfpqmvr.mongodb.net/?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+//   "mongodb+srv://Nader:nm3AfMQsHsSeRkGs@cluster.qfpqmvr.mongodb.net/?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 
